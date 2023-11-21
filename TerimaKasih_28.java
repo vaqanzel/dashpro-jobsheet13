77b085b962a0af79590a7cbf63af1d5f613f3662
vaqanzel <valengrehasta@gmail.com>
2023-11-21 11:24:29 +0700

    percobaan 1

---
+++ TerimaKasih_28.java
@@ -0,0 +1,11 @@
+
+public class TerimaKasih_28 {
+    public static void ucapTerimaKasih() {
+        System.out.println("Terima kasih telah menjadi guru terbaik di dunia.\n" +
+                "Anda telah menginspirasi saya untuk mencintai belajar dan membuat saya merasa bebas bertanya.");
+    }
+
+    public static void main(String[] args) {
+        ucapTerimaKasih();
+    }
+}
