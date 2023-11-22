9bc9f17274f054228d084e7be3f4b36afb966e6a
vaqanzel <valengrehasta@gmail.com>
2023-11-22 22:15:41 +0700

    percobaan 1

--- TerimaKasih_28.java
+++ TerimaKasih_28.java
@@ -1,11 +1,11 @@
 
 public class TerimaKasih_28 {
-    public static void ucapTerimaKasih() {
+    public static void ucapanTerimaKasih() {
         System.out.println("Terima kasih telah menjadi guru terbaik di dunia.\n" +
                 "Anda telah menginspirasi saya untuk mencintai belajar dan membuat saya merasa bebas bertanya.");
     }
 
     public static void main(String[] args) {
-        ucapTerimaKasih();
+        ucapanTerimaKasih();
     }
 }
