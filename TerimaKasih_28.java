6d3b183b05b43ae9090de74b645a2d532140922e
vaqanzel <valengrehasta@gmail.com>
2023-11-22 23:00:13 +0700

    modifikasi fungsi UcapanTambahan

--- TerimaKasih_28.java
+++ TerimaKasih_28.java
@@ -1,15 +1,13 @@
 public class TerimaKasih_28 {
-//     public static void UcapanTerimaKasih() {
-//         System.out.println("Thank you for being the best teacher in the world.\n"+
-//         "You inspired in me a love for learning and made me feel like i could ask you anything.");
-//     }
-    
-//     public static void main(String[] args) {
-//         UcapanTerimaKasih();
-//     }
-// }
- public static void main(String[] args) {
-            System.out.println("Thank you for being the best teacher in the world.\n" +
-                    "You inspired in me a love for learning and made me feel like I could ask you anything.");
-        }
+    public static void UcapanTerimaKasih() {
+        System.out.println("Thank you for being the best teacher in the world.\n"+
+        "You inspired in me a love for learning and made me feel like i could ask you anything.");
     }
+    public static void UcapanTambahan(String ucapan) {
+       System.out.println(ucapan);     }
+    public static void main(String[] args) {
+        UcapanTerimaKasih();
+        String ucapan = "Terima Kasih Pak.. Bu.. Semoga Sehat Selalu";
+        UcapanTambahan(ucapan);
+    }
+}
